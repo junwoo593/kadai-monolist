a5f96006f2b65099eebe135475b3476c3dffb7ff
JunWoo Choi <ec2-user@ip-172-31-11-104.us-east-2.compute.internal>
2018-06-14 01:51:08 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@
 
 Route::get('/', 'WelcomeController@index'); {
     return view('welcome');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\registerController@register')->name('signup.post');
 };
